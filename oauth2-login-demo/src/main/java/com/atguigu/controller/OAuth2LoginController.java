6fa4128974552cd710cc04949bfc9a93abdeac4e
shunpeng.hu <shunpeng.hu@9fzt.com.cn>
2024-04-07 16:33:59 +0800

    feat:oauth2协议的github所有第三方社交登录

---
+++ oauth2-login-demo/src/main/java/com/atguigu/controller/OAuth2LoginController.java
@@ -0,0 +1,27 @@
+package com.atguigu.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author: shunpeng.hu
+ * @date: 2024/4/7 16:26
+ */
+@Controller
+public class OAuth2LoginController {
+
+    @GetMapping("/")
+    public String index(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                        @AuthenticationPrincipal OAuth2User oauth2User) {
+        model.addAttribute("userName", oauth2User.getName());
+        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+        model.addAttribute("userAttributes", oauth2User.getAttributes());
+        return "index";
+    }
+
+}
