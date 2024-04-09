24d757ffc730fbd7f71b856b8e689cd12dfe8c91
hfnu_2010211112 <2942885033@qq.com>
2024-04-09 21:16:38 +0800

    update:通过内存用户、数据库用户信息配置认证登录

---
+++ security-demo/src/main/java/com/atguigu/config/DBUserDetailsManager.java
@@ -0,0 +1,89 @@
+package com.atguigu.config;
+
+import com.atguigu.entities.User;
+import com.atguigu.mapper.UserMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+/**
+ * 自定义数据库身份认证管理类
+ * @Author Husp
+ * @Date 2024/4/8 23:50
+ */
+@Slf4j
+public class DBUserDetailsManager implements UserDetailsManager, UserDetailsPasswordService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails updatePassword(UserDetails user, String newPassword) {
+        return user;  //  return null 出现空指针异常 Cannot invoke "org.springframework.security.core.userdetails.UserDetails.getAuthorities()"
+    }
+
+    @Override
+    public void createUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void deleteUser(String username) {
+
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        return false;
+    }
+
+    /**
+     * 通过数据库名校验用户信息是否合法
+     * @param username
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Collection<GrantedAuthority> authorities = new ArrayList<>(10);
+
+        //查询数据库的用户信息
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("username", username);
+        User user = userMapper.selectOne(wrapper);
+        if (Objects.isNull(user)) {
+            log.error("数据库中不存在该用户啦~");
+            throw new UsernameNotFoundException(username);
+        } else {
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUsername(), //用户名
+                    user.getPassword(), //密码
+                    (user.getEnabled() == 1),  //用户是否启用 true：启用
+                    true,   //帐户未过期 true：账户没有过期
+                    true,   //凭据是否未过期 true：凭据没有过期
+                    true,   //帐号是否未锁定 true：账户没有锁定
+                    authorities   //授予用户的权限
+            );
+        }
+    }
+}
--- security-demo/src/main/java/com/atguigu/config/WebSecurityConfig.java
+++ security-demo/src/main/java/com/atguigu/config/WebSecurityConfig.java
@@ -2,9 +2,7 @@ package com.atguigu.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @Author Husp
@@ -14,18 +12,30 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig {
 
     /**
-     * 注册基于内存用户信息的身份认证管理器到spring容器中
+     * 注册基于数据库用户信息的身份认证管理器到spring容器中
+     *
      * @return UserDetailsService用户信息对象
      */
     @Bean
     public UserDetailsService userDetailsService() {
-        //1.创建了一个基于内存用户信息的身份认证管理器
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        //3.使用内存用户的身份认证管理器创建包含用户名、密码、角色、权限信息的自定义用户对象
-        manager.createUser(
-                //2.自定义管理内存中的用户名信息、用户密码信息、用户角色信息、用户权限信息等。优先级：配置类的bean对象 > yml配置的属性
-                User.withDefaultPasswordEncoder().username("husp").password("password").roles("USER").build());
+        //1.创建了一个基于数据库用户信息的身份认证管理器
+        DBUserDetailsManager manager = new DBUserDetailsManager();
         return manager;
     }
+
+    /**
+     * 注册基于内存用户信息的身份认证管理器到spring容器中
+     * @return UserDetailsService用户信息对象
+     */
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        //1.创建了一个基于内存用户信息的身份认证管理器
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        //3.使用内存用户的身份认证管理器创建包含用户名、密码、角色、权限信息的自定义用户对象
+//        manager.createUser(
+//                //2.自定义管理内存中的用户名信息、用户密码信息、用户角色信息、用户权限信息等。优先级：配置类的bean对象 > yml配置的属性
+//                User.withDefaultPasswordEncoder().username("husp").password("password").roles("USER").build());
+//        return manager;
+//    }
 
 }
